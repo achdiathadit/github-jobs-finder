2898a5178639fa2fe4755eb56668b92858469b50
achdiathadit <achdiat.dev@gmail.com>
2020-07-25 14:34:38 +0700

    Updates

---
+++ precache-manifest.384bf72c9aeb225fc4786cdadbbc21fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c71f615300d40ec5d5058427494dc9",
+    "url": "/github-jobs-finder/index.html"
+  },
+  {
+    "revision": "6a3642194638a0ccdf3c",
+    "url": "/github-jobs-finder/static/js/2.ea2eba7e.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs-finder/static/js/2.ea2eba7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e045fc38946d2b7136",
+    "url": "/github-jobs-finder/static/js/main.5c1d56e8.chunk.js"
+  },
+  {
+    "revision": "91e310d0f2388516a55e",
+    "url": "/github-jobs-finder/static/js/runtime-main.225dd1e6.js"
+  }
+]);
