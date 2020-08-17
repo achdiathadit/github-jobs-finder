738212f5d36b723d952b8f0b9ee2bf3c3f0376ff
achdiathadit <achdiat.dev@gmail.com>
2020-08-18 06:30:37 +0700

    Update homepage

--- src/navbar.js
+++ src/navbar.js
@@ -3,8 +3,8 @@ import { Navbar } from 'react-bootstrap';
 
 export default function navbar() {
   return (
-    <Navbar bg='dark' variant='dark' className='navbar mb-3' sticky='top'>
-      <Navbar.Brand href='/'>GitHub Jobs Finder</Navbar.Brand>
+    <Navbar bg='dark' variant='dark' className='navbar mb-3'>
+      <Navbar.Brand href='/github-jobs-finder'>GitHub Jobs Finder</Navbar.Brand>
     </Navbar>
   );
 }
