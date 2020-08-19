059d2e6cfbf1b3abbf4d1509b11966c59a569348
achdiathadit <achdiat.dev@gmail.com>
2020-08-19 17:21:08 +0700

    Update app name

--- src/navbar.js
+++ src/navbar.js
@@ -5,7 +5,7 @@ import { Navbar } from 'react-bootstrap';
 export default function navbar() {
   return (
     <Navbar bg='dark' variant='dark' className='navbar mb-3'>
-      <Navbar.Brand href='/github-jobs-finder'>GitHub Jobs Finder</Navbar.Brand>
+      <Navbar.Brand href='/gitstreet'>GitStreet</Navbar.Brand>
     </Navbar>
   );
 }
